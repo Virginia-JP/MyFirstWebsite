b686b70423d640376824e1b0ee5f7ad7ccac5705
Virginia-JP <virginia2019lu@gmail.com>
2024-08-12 12:09:14 +0800

    add clock element

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-function updateClock() {
+// Clock Function
 
-    const now = new Date();
-    let hours =now.getHours();
-    const meridiem = hours >=12? "PM":"AM";
-    hours = hours % 12 || 12;
-    hours = hour.toString().padStart(2,0);
-    const minutes = now.getMinutes().toString().padStart(2,0);
-    const seconds = now.getSeconds().toString().padStart(2,0);
-    const timeString = `${hours}:${minutes}:${seconds}:{meridiem}`;
-    document.getElementById("clock").textContent = timeString;
+function updateClock(){
+  const now = new Date();
+
+  let hours = now.getHours();
+  const meridiem = hours >=12? "PM":"AM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2,0);
+
+  const minutes = now.getMinutes().toString().padStart(2,0);
+
+  const seconds = now.getSeconds().toString().padStart(2,0);
+
+  const month = now.getMonth()+1; 
+  const date = now.getDate();
+  const year = now.getFullYear();
+
+  const timeString1 = `${hours}:${minutes}:${seconds} ${meridiem} `;
+  const timeString2 = `${month}/${date}/${year}`;
+
+  document.getElementById("clock").textContent = timeString1;
+  document.getElementById("date").textContent = timeString2;
+
 }
 
 updateClock();
 setInterval(updateClock,1000);
+
+// Weather Function
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "";
+
+weatherForm.addEventListener("submit", async event => {
+
+})
+
+async function getWeatherData(city) {
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherherEmoji(weatherId){
+
+}
+
+function displayError(message){
+  
+}
